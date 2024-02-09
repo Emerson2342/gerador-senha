4702cd4c7ff125e857e0bf14c0efaf34b5e0ce27
Emerson <lyncoln_erc@hotmail.com>
2024-02-09 12:53:14 -0300

    feat: reinstalando dependências

--- src/components/BatLogo/BatLogo.tsx
+++ src/components/BatLogo/BatLogo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import { styles } from './BatLogoStyles';
 import batLogo from '../../../assets/batLogo.png'
